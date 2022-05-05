7dba793064e7fe27fa4b7ae321532fefef471a02
Krzysztof <77296221+kkSpeedFire@users.noreply.github.com>
2022-05-05 21:15:38 +0100

    pic-fixed

--- scripts/infoPage.js
+++ scripts/infoPage.js
@@ -1,20 +1,21 @@
-const sliders = documentSelectorAll(".info_img");
+const sliders = document.querySelectorAll(".info_img");
 
 const appearOptions = {
   threshold: 0,
   rootMargin: "0px 0px -250px 0px",
 };
-
 const appearOnScroll = new IntersectionObserver(function (
   entries,
-  appearOnScroll
+  observeOnScroll
 ) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
+      console.log("123");
       return;
     } else {
+      console.log("321");
       entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
+      observeOnScroll.unobserve(entry.target);
     }
   });
 },
